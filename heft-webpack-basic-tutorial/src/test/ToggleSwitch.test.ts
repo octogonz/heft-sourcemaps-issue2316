e05238252d7417c1bcad58f129efb84f6bdb6bd2
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-10-30 22:44:19 -0700

    Example of a test calling an imported library

--- heft-webpack-basic-tutorial/src/test/ToggleSwitch.test.ts
+++ heft-webpack-basic-tutorial/src/test/ToggleSwitch.test.ts
@@ -1,10 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-import { ToggleSwitch } from '../ToggleSwitch';
+import { TestClass } from "heft-web-rig-library-test";
 
-describe('ToggleSwitch', () => {
-  it('can be tested', () => {
-    expect(ToggleSwitch).toBeDefined();
+describe("ToggleSwitch", () => {
+  it("can be tested", () => {
+    const testClass = new TestClass();
+    expect(() => testClass.test()).not.toThrow();
   });
 });
